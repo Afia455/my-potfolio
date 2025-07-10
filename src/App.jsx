7b5d3c3b997ed4acd2846e5ec365696b7a78997a
Afia455 <mughalafia200@gmail.com>
2025-07-10 22:05:22 +0500

    Fix: corrected import paths for Linux case-sensitivity

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom"
-import { Home } from "./pages/home"
-import { Notfound } from "./pages/notfound"
+import { Home } from "./pages/Home"
+import { Notfound } from "./pages/Notfound"
 function App() {
 
   return (
